8bf7f0698ef93298ba952ff7a7c2b13350035f8b
kokyoungman <kokyoungman@naver.com>
2023-08-15 16:32:23 +0900

    K

--- body_viewer.js
+++ body_viewer.js
@@ -1,35 +1,59 @@
 // 자바스크립트 프로젝트 구현 - 인바디 다이어리 (body_viewer.js)
 
-
 // '바디 뷰어'에서 추가함
 
 const changeGender = () => {
-	switchGender();
+  switchGender();
 };
 
 const setDatas1 = () => {
-	const height = 180;		// 키
-	const weight = 76;		// 몸무게
-	const chest = 91.6;		// 가슴
-	const waist = 81.7;		// 허리
-	const hips = 96.5;		// 엉덩이
-	const inseam = 80;		// 가랑이부터 바닥까지
-	const exercise = 0;		// 일주일 운동 시간
-	setBody(height, weight, chest, waist, hips, inseam, exercise);
+  const height = 180; // 키
+  const weight = 76; // 몸무게
+  const chest = 91.6; // 가슴
+  const waist = 81.7; // 허리
+  const hips = 96.5; // 엉덩이
+  const inseam = 80; // 가랑이부터 바닥까지
+  const exercise = 0; // 일주일 운동 시간
+  setBody(height, weight, chest, waist, hips, inseam, exercise);
 };
 
 const setDatas2 = () => {
-	const height = 155;
-	const weight = 58;
-	const chest = 92.2;
-	const waist = 80.5;
-	const hips = 93.8;
-	const inseam = 82;
-	const exercise = 0;
-	setBody(height, weight, chest, waist, hips, inseam, exercise);
+  const height = 155;
+  const weight = 58;
+  const chest = 92.2;
+  const waist = 80.5;
+  const hips = 93.8;
+  const inseam = 82;
+  const exercise = 0;
+  setBody(height, weight, chest, waist, hips, inseam, exercise);
 };
 
-bodyViewerManager = new BodyViewerManager(document);
+const weightCanvasContext = document
+  .querySelector("#js-weight-chart-canvas")
+  .getContext("2d");
+const chestCanvasContext = document
+  .querySelector("#js-chest-chart-canvas")
+  .getContext("2d");
+const waistCanvasContext = document
+  .querySelector("#js-waist-chart-canvas")
+  .getContext("2d");
+const hipsCanvasContext = document
+  .querySelector("#js-hips-chart-canvas")
+  .getContext("2d");
+const upperArmCanvasContext = document
+  .querySelector("#js-upper-arm-chart-canvas")
+  .getContext("2d");
+const thighCanvasContext = document
+  .querySelector("#js-thigh-chart-canvas")
+  .getContext("2d");
+weightChart = new WeightChart(weightCanvasContext, [], [], [], [], []);
+chestChart = new WeightChart(chestCanvasContext, [], [], [], [], []);
+waistChart = new WeightChart(waistCanvasContext, [], [], [], [], []);
+hipsChart = new WeightChart(hipsCanvasContext, [], [], [], [], []);
+upperArmChart = new WeightChart(upperArmCanvasContext, [], [], [], [], []);
+thighChart = new WeightChart(thighCanvasContext, [], [], [], [], []);
+
+bodyViewerManager = new BodyViewerManager(document, weightChart, chestChart);
 bodyViewerManager.init();
 
 initCanvasIds("preview-viewer", "body-viewer");
--- manager/body_viewer_manager.js
+++ manager/body_viewer_manager.js
@@ -1,26 +1,75 @@
 // 자바스크립트 프로젝트 구현 - 인바디 다이어리 (body_viewer_manager.js)
 
-
 class BodyViewerManager {
-	constructor(document) {
-		// 타입 변경 버튼
-		this.typeChangeBtn1 = document.querySelector(".js-type-change-btn-1");
-		this.typeChangeBtn2 = document.querySelector(".js-type-change-btn-2");
-	}
+  constructor(document, worldWeightChart, koreaWeightChart) {
+    // 차트
+    this.worldWeightChart = worldWeightChart;
+    this.koreaWeightChart = koreaWeightChart;
 
-	// 타입 변경 버튼을 누름
-	handleTypeChangeBtn1 = (event) => {
-		event.preventDefault();
+    // 유저
+    this.userHeight;
 
-		location.href = "./";
-	}
-	handleTypeChangeBtn2 = (event) => {
-		event.preventDefault();
-	}
+    // 유저 선택 칸
+    this.userSelect = document.querySelector("select");
+    // 유저 선택 옵션들
+    this.userSelectOptions = document.querySelectorAll("option");
 
-	// 초기화함
-	init = () => {
-		this.typeChangeBtn1.addEventListener("click", this.handleTypeChangeBtn1);
-		this.typeChangeBtn2.addEventListener("click", this.handleTypeChangeBtn2);
-	}
+    // 유저 기본 데이터
+    this.userBaseData = document.querySelector(".js-base-data");
+
+    // 입력하는 칸들
+    this.inputs = [];
+    this.inputs.push(document.querySelector(".js-input-year"));
+    this.inputs.push(document.querySelector(".js-input-month"));
+    this.inputs.push(document.querySelector(".js-input-day"));
+    this.inputs.push(document.querySelector(".js-input-weight"));
+    this.inputs.push(document.querySelector(".js-input-chest"));
+    this.inputs.push(document.querySelector(".js-input-waist"));
+    this.inputs.push(document.querySelector(".js-input-hips"));
+    this.inputs.push(document.querySelector(".js-input-upper-arm"));
+    this.inputs.push(document.querySelector(".js-input-thigh"));
+
+    // 추가 버튼
+    this.addBtn = document.querySelector(".js-add-btn");
+
+    // 이전 버튼
+    this.beforeBtn = document.querySelector(".js-before-btn");
+
+    // 모드 변경 버튼
+    this.modeChangeBtn = document.querySelector(".js-mode-change-btn");
+
+    // 다음 버튼
+    this.nextBtn = document.querySelector(".js-next-btn");
+
+    // 타입 변경 버튼
+    this.typeChangeBtn1 = document.querySelector(".js-type-change-btn-1");
+    this.typeChangeBtn2 = document.querySelector(".js-type-change-btn-2");
+
+    // 모드 로컬 저장소
+    this.MODE_LS = "mode";
+
+    // 보여줄 데이터 갯수
+    this.MAX_VIEW_DATA_COUNT = 5;
+    this.viewDataStartIndex = 0;
+    this.viewDataCount = this.MAX_VIEW_DATA_COUNT;
+
+    // 시작 항목 인덱스
+    this.START_ITEM_INDEX = 4; // year, month, day, bmi를 제외하기 위해서 추가함
+  }
+
+  // 타입 변경 버튼을 누름
+  handleTypeChangeBtn1 = (event) => {
+    event.preventDefault();
+
+    location.href = "./";
+  };
+  handleTypeChangeBtn2 = (event) => {
+    event.preventDefault();
+  };
+
+  // 초기화함
+  init = () => {
+    this.typeChangeBtn1.addEventListener("click", this.handleTypeChangeBtn1);
+    this.typeChangeBtn2.addEventListener("click", this.handleTypeChangeBtn2);
+  };
 }
