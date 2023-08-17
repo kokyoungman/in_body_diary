e059becb5e1469b0a1f84d74c384f0e975fb8b1c
kokyoungman <kokyoungman@naver.com>
2023-08-17 16:21:32 +0900

    K

--- chart/c3/body_viewer_chart.js
+++ chart/c3/body_viewer_chart.js
@@ -1,55 +1,16 @@
 // 자바스크립트 프로젝트 구현 - 인바디 다이어리 (body_viewer_chart.js)
 
 class BodyViewerChart {
-  constructor(canvasContext, title, labels, values) {
-    const type = "bar";
-    const datasets = [];
-    const plugins = [ChartDataLabels];
-
-    const dataset = {
-      label: title,
-      data: values,
-      backgroundColor: "rgba(0, 0, 0, 0.2)",
-      barThickness: 20,
-    };
-    datasets.push(dataset);
-
-    const options = {
-      indexAxis: "y",
-      scales: {
-        x: {
-          beginAtZero: true,
-          max: 45,
-        },
+  constructor(canvasContext, titles, values) {
+    const chart = c3.generate({
+      bindto: "#js-chart",
+      data: {
+        columns: [
+          ["data1", 30, 200, 100, 400, 150, 250],
+          ["data2", 50, 20, 10, 40, 15, 25],
+        ],
       },
-      layout: {
-        padding: {
-          top: 55,
-        },
-      },
-      plugins: {
-        legend: {
-          display: false,
-        },
-        datalabels: {
-          color: "#020202",
-          anchor: "end",
-          align: "start",
-          formatter: (value, context) => {
-            return `${value}`;
-          },
-        },
-      },
-    };
-
-    this.baseChart = new BaseChart(
-      canvasContext,
-      type,
-      labels,
-      datasets,
-      options,
-      plugins
-    );
+    });
   }
 
   // 재설정함
